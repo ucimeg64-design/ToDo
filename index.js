56150cc5afeff05c6d53ddbb5f66e38587dbedf3
ucimeg64-design <ucimeg64@gmail.com>
2026-05-20 12:06:21 +0800

    second commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function addTask(){
     let inputEl = document.getElementById("taskInput");
     let li = document.createElement("li");
     li.innerText = inputEl.value;
+    li.style.fontSize = "20px";
     li.onclick = ()=> li.remove();
     taskListEl.appendChild(li);
     inputEl.value = "";
     let button = document.getElementById("nemehButton")
-    button.style.backgroundColor = "green";
     button.style.color = "white";
 
 }
